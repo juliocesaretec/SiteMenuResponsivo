a9b076ed5cfe79b5dd8209bcd2c781ae07382ef1
juliocesaretec <161508817+juliocesaretec@users.noreply.github.com>
2024-06-25 16:58:18 -0300

    lalalal

---
+++ mobile-navbar.js
@@ -0,0 +1,7 @@
+class MonileNavbar {
+    constructor(mobileMenu) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navlist);
+        this.navLinks = document.querySelectorAll(navlinks);
+    }
+}
